b6bee648e79cde36abeb8363cd7a0e7638c8d33f
Przemysław Jędral <jedral.przemek@gmail.com>
2021-05-04 20:13:53 +0200

    Add files via upload

---
+++ Cezar_cipher/CipherCezar.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cezar_cipher
+{
+    public class CipherCezar
+    {
+
+
+        public static char Cipher(char my_char, int key)
+        {
+            if (!char.IsLetter(my_char))
+            {
+                return my_char;
+            }
+
+            char start = char.IsUpper(my_char) ? 'A' : 'a';
+            return (char)((((my_char + key) - start) % 26) + start);
+
+        }
+
+
+        public static string Encipher(string input, int key)
+        {
+            string output = string.Empty;
+
+            foreach (char my_char in input)
+                output += Cipher(my_char, key);
+
+            return output;
+        }
+
+
+
+    }
+}
---
+++ Cezar_cipher/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Cezar_cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(CipherCezar.Encipher("Ala ma kotaa", 2));
+
+        }
+    }
+}
